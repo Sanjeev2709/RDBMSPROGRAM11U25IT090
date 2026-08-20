6191396f75382e8ad78d8b88c845c01f65862bca
Sanjeev2709 <sanjeevrajendran2709@gmail.com>
2026-08-20 14:01:57 +0530

    rdbms program 1 1U25IT090

--- solution.sql
+++ solution.sql
@@ -1,6 +1,6 @@
 Create database CollegeDB;
 USE CollegeDB;
-CREARE TABLE Department (DepartmentID INT(5)PRIMARY KEY,
+Create TABLE Department (DepartmentID INT(5)PRIMARY KEY,
   Departmentname VARCHAR(20),
   HOD VARCHAR(20)
 );
