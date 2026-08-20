b62a3845c6f9ad48c1c94c6e4015a71749cf9119
Sanjeev2709 <sanjeevrajendran2709@gmail.com>
2026-08-20 14:13:21 +0530

    RDBMS 1 PROGRAM 1U25IT090

--- solution.sql
+++ solution.sql
@@ -1,7 +1,10 @@
-Create database CollegeDB;
+CREATE DATABASE CollegeDB;
 USE CollegeDB;
-Create TABLE Department (DepartmentID INT(5)PRIMARY KEY,
-  Departmentname VARCHAR(20),
-  HOD VARCHAR(20)
+
+cREATE TABLE Department) 
+     DepartmentID INT(5) PRIMARY KEY,
+     Departmentname VARCHAR(20),
+     HOD VARCHAR(20)
 );
-describe department;
+
+DESCRIBE Department;
